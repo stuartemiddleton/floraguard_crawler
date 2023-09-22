8cfc5dac4f4a4d3cf236c246463791bca4ef60c6
Ishaipiriyan Karunakularatnam <I.Karunakularatnam@kew.org>
2023-09-22 16:50:26 +0100

    Crawler Deployment Successful: Instrcutions added

--- crawler/runner.py
+++ crawler/runner.py
@@ -17,7 +17,7 @@ from web_director.parser.custom_parser import create_custom_site, read_config, t
 def main():
     data = read_config()
     web = create_custom_site(data)
-    os.system("py -m scrapy crawl undercrawler -a url=" + web.get_general_start_page_url() + " -s DEPTH_LIMIT=" + str(data["depth"]) )
+    os.system("python -m scrapy crawl undercrawler -a url=" + web.get_general_start_page_url() + " -s DEPTH_LIMIT=" + str(data["depth"]) )
 
 if __name__ == '__main__':
     main()
